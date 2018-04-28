9f721fa741b8b440c4f288137c99aa3d42825b44
q00u <github@korv.us>
2018-04-27 21:27:50 -0700

    Assignment completed

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,19 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.BulletSpan;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,9 +50,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.placeholder_sandwich) //for shawarma
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -56,7 +64,39 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        //main name
+        TextView mName = (TextView) findViewById(R.id.name_tv);
+        mName.setText(sandwich.getMainName());
 
+        //description
+        TextView mDescription = (TextView) findViewById(R.id.description_tv);
+        mDescription.setText(sandwich.getDescription());
+
+        //origin
+        TextView mOrigin = (TextView) findViewById(R.id.origin_tv);
+        mOrigin.setText(sandwich.getPlaceOfOrigin());
+
+        //aka
+        TextView mAlsoKnownAs = (TextView) findViewById(R.id.also_known_tv);
+        List<String> alsoKnownAs = sandwich.getAlsoKnownAs();
+        if (0<alsoKnownAs.size()) {//skip in case it's empty
+            mAlsoKnownAs.setText(sandwich.getAlsoKnownAs().get(0));
+            for (int i = 1; i < sandwich.getAlsoKnownAs().size(); ++i) {
+                mAlsoKnownAs.append(", " + sandwich.getAlsoKnownAs().get(i));
+            }
+        }
+
+        //ingredients list with fancy bullets
+        //based on https://stackoverflow.com/questions/4992794/how-to-add-bulleted-list-to-android-application
+        // and https://developer.android.com/reference/android/text/style/BulletSpan
+        TextView mIngredients = (TextView) findViewById(R.id.ingredients_tv);
+        SpannableStringBuilder ssb = new SpannableStringBuilder();
+        for (String i : sandwich.getIngredients()) {
+            SpannableString ss = new SpannableString(i+"\n");
+            ss.setSpan(new BulletSpan(8),0,i.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+            ssb.append(ss);
+        }
+        mIngredients.setText(ssb);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,50 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        try {
+            //JSON root
+            JSONObject sandwichJson = new JSONObject(json);
+            //name object
+            JSONObject name = sandwichJson.getJSONObject("name");
+            //name sub-objects
+            String mainName = name.getString("mainName");
+            JSONArray alsoKnownAsJson = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i<alsoKnownAsJson.length(); ++i) {
+                alsoKnownAs.add(alsoKnownAsJson.optString(i));
+            }
+            //origin
+            String placeOfOrigin = sandwichJson.getString("placeOfOrigin");
+            //description
+            String description = sandwichJson.getString("description");
+            //image URI
+            String image = sandwichJson.getString("image");
+            //list of ingredients
+            JSONArray ingredientsJson = sandwichJson.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i<ingredientsJson.length(); ++i) {
+                ingredients.add(ingredientsJson.optString(i));
+            }
+            //if no errors by this point, build sandwich and return it
+            return new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
+        } catch (JSONException err) {
+            //something went wrong, bail out!
+            Log.e("JsonUtils", "Error in JsonUtils ", err);
+            return null;
+        }
     }
 }
